145f8c4872073a79e0bced07e82628a461390d0a
apashintsev <av.pashintsev@gmail.com>
2026-02-13 19:37:47 +0300

    Add FetchXPOKRequest and FetchXPOKResponse messages with market maker details

--- scripts/deployReturnWallet.ts
+++ scripts/deployReturnWallet.ts
@@ -1,9 +1,14 @@
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 import { ReturnWallet } from '../wrappers/ReturnWallet';
 import { NetworkProvider } from '@ton/blueprint';
 
+//npx blueprint run deployReturnWallet --testnet --mnemonic
 export async function run(provider: NetworkProvider) {
-    const returnWallet = provider.open(await ReturnWallet.fromInit());
+    let launchJettonMasterAddress = 'EQC8Xo9n7sHj2mLh3u5Zt1v6z5c5s5s5s5s5s5s5s5s5s5s5s5s5s5';
+    let mmAddress = 'EQC8Xo9n7sHj2mLh3u5Zt1v6z5c5s5s5s5s5s5s5s5s5s5s5s5s5s5';
+    const returnWallet = provider.open(
+        await ReturnWallet.fromInit(Address.parse(launchJettonMasterAddress), Address.parse(mmAddress)),
+    );
 
     await returnWallet.send(
         provider.sender(),
@@ -13,7 +18,7 @@ export async function run(provider: NetworkProvider) {
         {
             $$type: 'Deploy',
             queryId: 0n,
-        }
+        },
     );
 
     await provider.waitForDeploy(returnWallet.address);
